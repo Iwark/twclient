706594a71c564da8f6c402e59bf6290d7a50ef2b
Iwark <iwark02@gmail.com>
2014-04-18 15:03:59 +0900

    lunch

--- app.js
+++ app.js
@@ -9,82 +9,119 @@ var T = new Twit({
   , access_token_secret:  'ewu1XPZzyXDNDwez8sSNHFdYYnkvugmNq6ZxwNDTBcmia'
 })
 
+var mongoose = require('mongoose');
+var schema = require('./schema.js');
+for (var key in schema){
+  mongoose.model(key, schema[key]);
+}
+var Follower = mongoose.model('Follower');
+mongoose.connect('mongodb://localhost/twclient');
+
 //
 //  tweet 'hello world!'
-//
-T.post('statuses/update', { status: 'hello world!' }, function(err, reply) {
-  //  ...
-})
+// //
+// T.post('statuses/update', { status: 'hello world!' }, function(err, reply) {
+//   //  ...
+// })
 
-//
-//  search twitter for all tweets containing the word 'banana' since Nov. 11, 2011
-//
-T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, reply) {
-  //  ...
-})
+// //
+// //  search twitter for all tweets containing the word 'banana' since Nov. 11, 2011
+// //
+// T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, reply) {
+//   //  ...
+// })
 
-//
-//  get the list of user id's that follow @tolga_tezel
-//
-T.get('followers/ids', { screen_name: 'tolga_tezel' },  function (err, reply) {
-  //  ...
-})
+// //
+// //  get the list of user id's that follow @tolga_tezel
+// //
 
-//
-//  retweet a tweet with id '343360866131001345'
-//
-T.post('statuses/retweet/:id', { id: '343360866131001345' }, function (err, reply) {
-  //  ...
-})
+// フォロワーチェック
+T.get('followers/ids', { screen_name: 'rzmrumgxx' },  function (err, reply) {
+  if(!err){
+    reply["ids"].forEach(function(follower_id){
+      Follower.findOne({ follower_id: follower_id },function(err, f){
+        if(err) console.log(err);
+        else{
+          if(f){
+            //既にデータベースに存在する場合
+          }else{
+            var follower = new Follower({
+              follower_id: follower_id,
+              step: 1
+            });
+            follower.save(function(err){
+              if(err) console.log(err);
+              else{
+                T.post('direct_messages/new', { user_id:follower_id, text:'こんにちは！' }, function(err, reply){
+                  console.log(err);
+                  console.log(reply);
+                });
+              }
+            });
+          }
+        }
+      });
+    });
+  }
+  console.log(err);
+  console.log(reply);
+});
 
-//
-//  destroy a tweet with id '343360866131001345'
-//
-T.post('statuses/destroy/:id', { id: '343360866131001345' }, function (err, reply) {
-  //  ...
-})
+// //
+// //  retweet a tweet with id '343360866131001345'
+// //
+// T.post('statuses/retweet/:id', { id: '343360866131001345' }, function (err, reply) {
+//   //  ...
+// })
 
-//
-// get `funny` twitter users
-//
-T.get('users/suggestions/:slug', { slug: 'funny' }, function (err, reply) {
-  //  ...
-})
+// //
+// //  destroy a tweet with id '343360866131001345'
+// //
+// T.post('statuses/destroy/:id', { id: '343360866131001345' }, function (err, reply) {
+//   //  ...
+// })
 
-//
-//  stream a sample of public statuses
-//
-var stream = T.stream('statuses/sample')
+// //
+// // get `funny` twitter users
+// //
+// T.get('users/suggestions/:slug', { slug: 'funny' }, function (err, reply) {
+//   //  ...
+// })
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet)
-})
+// //
+// //  stream a sample of public statuses
+// //
+// var stream = T.stream('statuses/sample')
 
-//
-//  filter the twitter public stream by the word 'mango'.
-//
-var stream = T.stream('statuses/filter', { track: 'mango' })
+// stream.on('tweet', function (tweet) {
+//   console.log(tweet)
+// })
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet)
-})
+// //
+// //  filter the twitter public stream by the word 'mango'.
+// //
+// var stream = T.stream('statuses/filter', { track: 'mango' })
 
-//
-// filter the public stream by the latitude/longitude bounded box of San Francisco
-//
-var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
+// stream.on('tweet', function (tweet) {
+//   console.log(tweet)
+// })
 
-var stream = T.stream('statuses/filter', { locations: sanFrancisco })
+// //
+// // filter the public stream by the latitude/longitude bounded box of San Francisco
+// //
+// var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet)
-})
+// var stream = T.stream('statuses/filter', { locations: sanFrancisco })
 
-//
-// filter the public stream by english tweets containing `#apple`
-//
-var stream = T.stream('statuses/filter', { track: '#apple', language: 'en' })
+// stream.on('tweet', function (tweet) {
+//   console.log(tweet)
+// })
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet)
-})
+// //
+// // filter the public stream by english tweets containing `#apple`
+// //
+// var stream = T.stream('statuses/filter', { track: '#apple', language: 'en' })
+
+// stream.on('tweet', function (tweet) {
+//   console.log(tweet)
+// })
