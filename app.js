b57fb9564353003cc0b1191986465002b55bb89a
Iwark <iwark02@gmail.com>
2014-04-20 19:42:52 +0900

    beta

--- app.js
+++ app.js
@@ -1,127 +1,179 @@
-// https://github.com/ttexel/twit
-
-var Twit = require('twit')
-
-var T = new Twit({
-    consumer_key:         '9w8aeT46ZRDWOZkLJVw1A'
-  , consumer_secret:      'deuO72B43ubDWMVje5mDPXfebLTCFtzWz0upykSXx4'
-  , access_token:         '768942398-LUJFcyWaDo48R17OEsRNJXgrkVnnxMA100xjNH8i'
-  , access_token_secret:  'ewu1XPZzyXDNDwez8sSNHFdYYnkvugmNq6ZxwNDTBcmia'
-})
-
+var Twit = require('twit');
 var mongoose = require('mongoose');
 var schema = require('./schema.js');
+var async = require('async');
+
+// 使用するTwitterアカウントと、その設定項目
+var SCREEN_NAME = 'sayakarzmrumgx'
+var T = new Twit({
+    consumer_key:         'y9MH162TvhljOcLdlG9m4mkbc'
+  , consumer_secret:      'epMyUD0bdPXTnB8YsCEvM287BVr26rrMIDHYMHIMWtMFM3fSIZ'
+  , access_token:         '2454429072-z4ieC1hnsjd5JVm5ss2X9Ly8YwJV5Y2BnGYRckD'
+  , access_token_secret:  'eiQI8iSkWb2i5Ds2htyQWVJx2Ostw37eb3gq8wWfUfrdD'
+});
+
+// ステップ定数（段階）
+var FOLLOW_CAME = 1;  // フレンドからのフォローを検知した段階
+var DM1_SENT = 2;     // １段階目のDMを送信した状態
+var RP1_CAME = 3;     // DMに対する１度目の返信を検知した段階
+var DM2_SENT = 4;     // ２段階目のDMを送信した状態
+var RP2_CAME = 5;     // DMに対する２度目の返信を検知した段階
+var DM3_SENT = 6;     // ３段階目のDMを送信した状態
+var RP3_CAME = 7;     // DMに対する３度目の返信を検知した段階
+var DM4_SENT = 8;     // ４段階目のDMを送信した状態
+var RP4_CAME = 9;     // DMに対する４度目の返信を検知した段階
+var DM5_SENT = 10;     // ５段階目のDMを送信した状態
+
+// 15分間に送信するメッセージの最大数
+var MAX_NUM_OF_DM = 2;
+
+// ステップごとに送信するDMの内容
+var DIRECT_MESSAGES = [
+  {
+    step: FOLLOW_CAME,
+    message: "リフォローありがと♡新しく作りなおしたの(ू•‧̫•ू⑅) 仲良くしてね･ﾟﾟ(p>д<q)ﾟﾟ･"
+  },
+  {
+    step: RP1_CAME,
+    message: "返事ありがと꒰ ︠ु௰•꒱ु♡  最近、関西にきたんだ(´∀｀艸)♡  友達少ないから仲良くしてね･ﾟﾟ(p>д<q)ﾟﾟ･  どこらへん住んでるの？( •ॢ◡-ॢ)-♡"
+  },
+  {
+    step: RP2_CAME,
+    message: "そうなんだ( •ॢ◡-ॢ)-♡お仕事なにしてるの？(｡≧Д≦｡)    ウチは夜職してるよ(｡>ω<｡) 偏見もってない(*´･д･)?"
+  },
+  {
+    step: RP3_CAME,
+    message: "ウチは夜っていっても風俗なんだけどね꒰ ︠ु௰•꒱ु"
+  },
+  {
+    step: RP4_CAME,
+    message: ""
+  }
+];
+
+// 処理の繰り返し間隔
+var INTERVAL = 0.5 * 60 * 1000; //15分
+
+// データベースの初期設定
 for (var key in schema){
   mongoose.model(key, schema[key]);
 }
 var Follower = mongoose.model('Follower');
 mongoose.connect('mongodb://localhost/twclient');
 
-//
-//  tweet 'hello world!'
-// //
-// T.post('statuses/update', { status: 'hello world!' }, function(err, reply) {
-//   //  ...
-// })
+var lastSentDirectMessageID;
 
-// //
-// //  search twitter for all tweets containing the word 'banana' since Nov. 11, 2011
-// //
-// T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, reply) {
-//   //  ...
-// })
+setInterval(function(){
 
-// //
-// //  get the list of user id's that follow @tolga_tezel
-// //
+  // 送信待ちの段階にあるフォロワーへのDM送信
+  var mesNum = 0;
+  async.each([RP4_CAME, RP3_CAME, RP2_CAME, RP1_CAME, FOLLOW_CAME], function(step, callback){
+    Follower.find({ step: step }, function(err, followers){
+      if(err) console.log(err);
+      else{
+        // 送信するメッセージ
+        var message;
+        DIRECT_MESSAGES.forEach(function(mes){
+          if(mes["step"] == step){
+            message = mes["message"];
+            return;
+          }
+        });
+        followers.forEach(function(follower){
+          if(mesNum > MAX_NUM_OF_DM) return;
+          T.post('direct_messages/new', { user_id:follower.follower_id, text:message }, function(err, reply){
+            if(err) console.log(err);
+            else console.log("step" + step + " DM done :" + reply);
+          });
+          follower.step++;
+          follower.save(function(err){ if(err) console.log(err); });
+          mesNum++;
+        });
+      }
+      callback();
+    });
+  });
 
-// フォロワーチェック
-T.get('followers/ids', { screen_name: 'rzmrumgxx' },  function (err, reply) {
-  if(!err){
-    reply["ids"].forEach(function(follower_id){
-      Follower.findOne({ follower_id: follower_id },function(err, f){
-        if(err) console.log(err);
-        else{
-          if(f){
-            //既にデータベースに存在する場合
-          }else{
-            var follower = new Follower({
-              follower_id: follower_id,
-              step: 1
-            });
-            follower.save(function(err){
-              if(err) console.log(err);
-              else{
-                T.post('direct_messages/new', { user_id:follower_id, text:'こんにちは！' }, function(err, reply){
-                  console.log(err);
-                  console.log(reply);
-                });
+  // DMの検出
+  var param = { include_entities: false, skip_status: true, since_id: "457450918217130000" };
+  if(lastSentDirectMessageID) param["since_id"] = lastSentDirectMessageID;
+  console.log(param["since_id"]);
+  T.get('direct_messages', param, function(err, directMessages){
+    if(err) console.log(err);
+    else{
+      if(directMessages && directMessages.length > 0){
+        lastSentDirectMessageID = directMessages[0]["id"];
+        // DMを送った人の段階を変更する
+        async.each(directMessages, function(directMessage, callback){
+          if(!directMessage["sender_id"] || !directMessage["id"]) callback();
+          else{
+            Follower.findOne({ follower_id: directMessage["sender_id"]}, function(err, follower){
+              if(!err && follower){
+                if(follower.step == DM1_SENT || follower.step == DM2_SENT || follower.step == DM3_SENT || follower.step == DM4_SENT){
+                  if(follower.last_sent_at){
+                    var lastDate = new Date(follower.last_sent_at);
+                    if(directMessage["created_at"] - lastDate <= 10) return;
+                  }
+                  follower.step++;
+                  follower.last_sent_at = directMessage["created_at"];
+                  follower.save(function(err){ if(err) console.log(err); callback(); });
+                }else callback();
+              }else{
+                if(err) console.log(err);
+                callback();
               }
             });
           }
-        }
+        });
+      }
+      console.log("directMessages :" + JSON.stringify(directMessages));
+    }
+  });
+
+  // リフォローの検出
+  async.waterfall([
+    // フレンドリストの取得
+    function(callback){
+      T.get('friends/ids', { screen_name: SCREEN_NAME }, function(err, reply){
+        var friends = [];
+        if(!err) friends = reply["ids"];
+        console.log("got friend_list.");
+        callback(err, friends);
       });
-    });
-  }
-  console.log(err);
-  console.log(reply);
-});
+    },
+    // フレンドリストに存在するフォロワーの取得
+    function(friends, callback){
+      T.get('followers/ids', { screen_name: SCREEN_NAME },  function (err, reply) {
+        var followers = [];
+        if(!err) followers = reply["ids"].filter(function(follower_id){
+          for(var i=0; i<friends.length; i++){
+            if(parseInt(follower_id) == parseInt(friends[i])) return true;
+          }
+          return false;
+        });
+        console.log("got follower_list.");
+        callback(err, followers);
+      });
+    },
+    // データベースに存在していなければ作成
+    function(followers, callback){
+      async.each(followers, function(follower_id, a_callback){
+        Follower.findOne({ follower_id: follower_id },function(err, follower){
+          if(!err && !follower){
+            //データベースに存在していない場合
+            var newFollower = new Follower({ follower_id: follower_id, step: FOLLOW_CAME });
+            newFollower.save(function(err){
+              if(err) console.log(err);
+              a_callback();
+            });
+          }
+        });
+      });
+      callback(null, 'done');
+    }
+  ], function(err,result){
+    if(err) console.log(err);
+    console.log('series all done. ' + result);
+  });
 
-// //
-// //  retweet a tweet with id '343360866131001345'
-// //
-// T.post('statuses/retweet/:id', { id: '343360866131001345' }, function (err, reply) {
-//   //  ...
-// })
-
-// //
-// //  destroy a tweet with id '343360866131001345'
-// //
-// T.post('statuses/destroy/:id', { id: '343360866131001345' }, function (err, reply) {
-//   //  ...
-// })
-
-// //
-// // get `funny` twitter users
-// //
-// T.get('users/suggestions/:slug', { slug: 'funny' }, function (err, reply) {
-//   //  ...
-// })
-
-// //
-// //  stream a sample of public statuses
-// //
-// var stream = T.stream('statuses/sample')
-
-// stream.on('tweet', function (tweet) {
-//   console.log(tweet)
-// })
-
-// //
-// //  filter the twitter public stream by the word 'mango'.
-// //
-// var stream = T.stream('statuses/filter', { track: 'mango' })
-
-// stream.on('tweet', function (tweet) {
-//   console.log(tweet)
-// })
-
-// //
-// // filter the public stream by the latitude/longitude bounded box of San Francisco
-// //
-// var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
-
-// var stream = T.stream('statuses/filter', { locations: sanFrancisco })
-
-// stream.on('tweet', function (tweet) {
-//   console.log(tweet)
-// })
-
-// //
-// // filter the public stream by english tweets containing `#apple`
-// //
-// var stream = T.stream('statuses/filter', { track: '#apple', language: 'en' })
-
-// stream.on('tweet', function (tweet) {
-//   console.log(tweet)
-// })
+}, INTERVAL);
--- schema.js
+++ schema.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 
 var FollowerSchema = new Schema({
   follower_id: {type:Number, default: 0},
-  step: {type:Number, default: 0}
+  step: {type:Number, default: 0},
+  last_sent_at: {type:String}
 });
 
 module.exports = {
